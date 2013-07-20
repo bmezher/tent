36ecddd2bf37cb2b34c75eb718917760857e8cab
Boris <mboris@gmail.com>
2013-07-20 16:57:01 -0400

    Added initial vi configuration and installation notes.

---
+++ installation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Installation file for new system, use as desired.
+
+## 1. Set up the tent directory and checkout code from github
+## 2. Link ~/.profile to $TENT/config/bash.profile, or source it directly
+## 3. Install homebrew and helper programms
+#      ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+#      brew install ack
+## 4. Set up Vim:
+##     4a. Install MacVim
+##     4b. Install Vim configuration   ######## sh <(curl https://j.mp/spf13-vim3 -L)
+#          git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+#          ln -s ~/.tent/config/vim.vimrc ~/.vimrc
+
